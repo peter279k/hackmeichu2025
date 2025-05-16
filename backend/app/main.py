000505f40edec824a45e7b5884c205b6369765c3
peter279k <peter279k@gmail.com>
2025-05-17 03:12:50 +0800

    Change main.py to include carbon_calculator_router

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,10 @@ from fastapi import FastAPI
 
 
 description = '''
-FHIR Generator is used to create the specific FHIR resources
+Carbon Emission Generator is used to calculate carbon emission result
 '''
 app = FastAPI(
-    title='FHIR Generator',
+    title='Carbon Calculator',
     description=description,
     version='1.0',
     contact={
@@ -16,22 +16,4 @@ app = FastAPI(
     }
 )
 
-app.include_router(claim_router)
-app.include_router(location_router)
-app.include_router(document_reference_router)
-app.include_router(imaging_study_router)
-app.include_router(procedure_router)
-app.include_router(medication_request_router)
-app.include_router(composition_router)
-app.include_router(coverage_router)
-app.include_router(care_plan_router)
-app.include_router(patient_router)
-app.include_router(organization_router)
-app.include_router(practitioner_router)
-app.include_router(practitioner_role_router)
-app.include_router(goal_router)
-app.include_router(condition_router)
-app.include_router(service_request_router)
-app.include_router(observation_router)
-app.include_router(encounter_router)
-app.include_router(diagnostic_report_router)
+app.include_router(carbon_calculator_router)
