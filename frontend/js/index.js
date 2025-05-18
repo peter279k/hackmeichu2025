bfd3212184c4fff9812df0bc407fe5051a623c26
peter279k <peter279k@gmail.com>
2025-05-18 22:04:37 +0800

    Complete get electric carbon factor from backend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,10 +1,21 @@
 async function fetchElectricCarbonFactor() {
-    jsonUrl = 'http://localhost:8000/api/v1/electric_carbon_factor';
+    let apiUrl = $('#api-endpoint').val();
+    apiUrl = '/api/v1/electric_carbon_factor';
     await $.ajax({
-        url: jsonUrl,
-                method: 'GET',
-                headers: {'Accept': 'application/json'},
-            }).done((data) => {
-                console.log(JSON.parse(data));
-            });
+        url: apiUrl,
+        method: 'GET',
+        headers: {'Accept': 'application/json'},
+    }).done((data) => {
+        console.log(JSON.parse(data));
+    }).error(error) => {
+        Swal.fire({
+            'title': '錯誤',
+            'text': error,
+            'icon': 'error',
+        });
+    };
+}
+
+async function calculateElectricCarbonFactor() {
+
 }
