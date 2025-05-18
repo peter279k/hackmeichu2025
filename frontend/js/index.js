ae2b781e51f4897c02c1f31b856f983ff2b9aec2
peter279k <peter279k@gmail.com>
2025-05-18 22:35:46 +0800

    Add JS about calculate electric carbon factor

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,7 +6,12 @@ async function fetchElectricCarbonFactor() {
         method: 'GET',
         headers: {'Accept': 'application/json'},
     }).done((data) => {
-        console.log(data);
+        let electricCarbonFactor = data.data.data;
+        let optionString = '';
+        for (let index=0; index<electricCarbonFactor.length; index++) {
+            optionString += `<option value="${electricCarbonFactor[index]['co2e']}">${electricCarbonFactor[index]['co2e']}(${electricCarbonFactor[index]['year']}年)</option>`;
+        }
+        $('#electric-carbon-factor').html(optionString);
     }).fail((jqXHR, textStatus, error) => {
         console.log(jqXHR);
         Swal.fire({
@@ -18,5 +23,24 @@ async function fetchElectricCarbonFactor() {
 }
 
 async function calculateElectricCarbonFactor() {
-
+    let apiUrl = $('#api-endpoint').val();
+    apiUrl = '/api/v1/calculate_electric';
+    await $.ajax({
+        url: apiUrl,
+        method: 'POST',
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+        data: {
+            'activity_data': Number($('#kwh-value').val()),
+            'factor': Number($('#electric-carbon-factor').val()),
+        },
+    }).done((data) => {
+        console.log(data);
+    }).fail((jqXHR, textStatus, error) => {
+        console.log(jqXHR);
+        Swal.fire({
+            'title': `錯誤，${textStatus}`,
+            'text': error,
+            'icon': 'error',
+        });
+    });
 }
